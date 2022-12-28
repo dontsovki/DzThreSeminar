245494ac3b5638370941467e4f9294e472e03a9d
Artem.don <dontsovki@gmail.com>
2022-12-28 22:19:03 +0300

    Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,19 +1,19 @@
 ﻿// Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве...
-Console.Write("введите координату x1");
+Console.Clear();
+Console.Write("введите координату x1 - ");
 int num_x1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("введите координату y1");
+Console.Write("введите координату y1 - ");
 int num_y1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("введите координату z1");
+Console.Write("введите координату z1 - ");
 int num_z1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("введите координату x2");
+Console.Write("введите координату x2 - ");
 int num_x2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("введите координату y2");
+Console.Write("введите координату y2 - ");
 int num_y2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("введите координату z2");
+Console.Write("введите координату z2 - ");
 int num_z2 = Convert.ToInt32(Console.ReadLine());
 
-void num1()
-{
+double d = Math.Sqrt(Math.Pow(num_x1-num_x2, 2) + Math.Pow(num_y1-num_y2, 2) + Math.Pow(num_z1-num_z2, 2));
 
-}
+Console.WriteLine($"d={d:f5}");
 
